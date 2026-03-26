3b6c6139a198284354d7829a3e3f15f1a1cbf057
Kimiblock Moe <kimiblock@icloud.com>
2026-03-26 21:48:27 +0800

    exit on no config

--- cmdline.go
+++ cmdline.go
@@ -6,6 +6,7 @@ func cmdline(args []string) Config {
 	var ret Config
 	var skip int
 	var dryRun bool
+	var hasConf bool
 	for idx := range args {
 		if skip > 0 {
 			skip--
@@ -18,11 +19,15 @@ func cmdline(args []string) Config {
 					warn.Fatalln("--config requires an argument")
 				}
 				ret = readConf(args[idx + 1])
+				hasConf = true
 			case "--dry-run":
 				dryRun = true
 			default:
 				warn.Println("Unknown argument:", args[idx])
 		}
+	}
+	if ! hasConf {
+		warn.Fatalln("A configuration is needed! Supply via --config")
 	}
 	if dryRun {
 		logger.Println(ret)
